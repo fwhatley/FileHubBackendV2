f0a1940d449cbd5db5dcedf89d2d8059be8235b4
Fredy Whatley <fwhatley@frontlineed.com>
2018-09-18 15:42:39 -0400

    init

--- FileHubBackendV2/Program.cs
+++ FileHubBackendV2/Program.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace FileHubBackendV2
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) // dotnet core is a console app
         {
             CreateWebHostBuilder(args).Build().Run();
         }
