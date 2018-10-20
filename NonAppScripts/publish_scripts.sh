d76faeb76abb654f9df4f2bccd76d77738106801
Fredy Whatley <fwhatley@frontlineed.com>
2018-10-20 19:00:20 -0400

    update scripts

--- NonAppScripts/publish_scripts.sh
+++ NonAppScripts/publish_scripts.sh
@@ -2,6 +2,6 @@
 
 dotnet publish
 rm -rf /var/FileHubBackendV2
-sudo cp -a ~/FileHubBackendV2/bin/Debug/netcoreapp1.0/publish /var/FileHubBackendV2
+sudo cp -a ~/FileHubBackendV2/FileHubBackendV2/bin/Debug/netcoreapp2.1/publish/ /var/FileHubBackendV2
 sudo /usr/bin/dotnet /var/FileHubBackendV2/FileHubBackendV2.dll --server.urls:https://*:5000
 
