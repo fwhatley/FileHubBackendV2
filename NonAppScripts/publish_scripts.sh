c71a63868e17f3dcdcee307a3f31e7082da33b2b
Fredy Whatley <fwhatley@frontlineed.com>
2018-10-20 19:10:37 -0400

    update script

--- NonAppScripts/publish_scripts.sh
+++ NonAppScripts/publish_scripts.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -x #echo on
 
+echo "INFO - publishing app: FileHubBackendV2"
 dotnet publish
+
+echo "INFO - deleting old app: FileHubBackendV2"
 rm -rf /var/FileHubBackendV2
+
+echo "INFO - copying new app to be served: FileHubBackendV2"
 sudo cp -a ~/FileHubBackendV2/FileHubBackendV2/bin/Debug/netcoreapp2.1/publish/ /var/FileHubBackendV2
+
+echo "INFO - serving app: FileHubBackendV2"
 sudo /usr/bin/dotnet /var/FileHubBackendV2/FileHubBackendV2.dll --server.urls:https://*:5000
 
