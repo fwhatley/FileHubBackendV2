ae3cca98ae2c735b13c2e2c1b6e160fc2fe1a66a
Fredy Whatley <fwhatley@frontlineed.com>
2018-10-24 22:20:24 -0400

    update publish script to take into account supervisor

--- publish_scripts.sh
+++ publish_scripts.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #set -x #echo on
 
-echo "INFO - publishing app: FileHubBackendV2"
-dotnet publish
+echo "INFO - stop supervisor to avoid port used errors"
+sudo service supervisor stop
 
 echo "INFO - deleting old app: FileHubBackendV2"
 rm -rf /var/FileHubBackendV2
+
+echo "INFO - publishing app: FileHubBackendV2"
+dotnet publish
 
 echo "INFO - copying new app to be served: FileHubBackendV2"
 sudo cp -a ~/FileHubBackendV2/FileHubBackendV2/bin/Debug/netcoreapp2.1/publish/ /var/FileHubBackendV2
 
 echo "INFO - serving app: FileHubBackendV2"
 sudo /usr/bin/dotnet /var/FileHubBackendV2/FileHubBackendV2.dll --server.urls:https://*:5000
+
+echo "INFO - start supervisor so FileHubApi never goes down"
+sudo service supervisor start
 
