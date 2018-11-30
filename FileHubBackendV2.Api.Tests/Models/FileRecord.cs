b37254a7453c97dd9534957785a7e95e23bc2e19
Fredy Whatley <fwhatley@frontlineed.com>
2018-11-30 01:16:00 -0500

    add Api test project

---
+++ FileHubBackendV2.Api.Tests/Models/FileRecord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FileHubBackendV2.Api.Tests.Models
+{
+    // Dto: prefix for data transfer object, have no business logic
+    // Bo : prefix for business object needs to have business logic, eg., required fields
+    // none : no prefix means that that POCO is used only for db table generation. 
+    //  - Each prop should math a column
+    //  - don't add a property if it doesn't exist in the table
+    // referenced used: https://www.codeproject.com/Articles/1050468/Data-Transfer-Object-Design-Pattern-in-Csharp
+    public class FileRecord
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Tags { get; set; }
+        public DateTime CreatedUtc { get; set; }
+        public DateTime UpdatedUtc { get; set; }
+        public DateTime DeletedUtc { get; set; }
+    }
+
+}
---
+++ FileHubBackendV2.Api.Tests/Tests/FilesTests.cs
@@ -0,0 +1,62 @@
+﻿using FileHubBackendV2.Api.Tests.Models;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyApp.Tests
+{
+    [TestFixture]
+    public class FilesTests
+    {
+        private readonly string baseUrl = "http://localhost:5000";
+
+        [Test]
+        public async Task GetFiles_ShouldReturnOk()
+        {
+            var url = $"{baseUrl}/api/files";
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+            var fhFile = await response.Content.ReadAsAsync<List<FhFile>>();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        }
+
+        [Test]
+        public async Task PostFile_ShouldReturnOk()
+        {
+            var url = $"{baseUrl}/api/files";
+            var imageInBytes = File.ReadAllBytes("./InputTestFiles/fileName1.jpg"); // ./ instead of ../ because tests are run from root folder in debug or release mode
+
+            // set up conten to upload
+            HttpContent fileName = new StringContent("fileName"); 
+            HttpContent fileRecord = new StringContent("fileRecord"); 
+            HttpContent file = new StreamContent(new MemoryStream(imageInBytes));
+
+
+            using (var client = new HttpClient())
+            {
+                using (var content = new MultipartFormDataContent("Upload----" + System.DateTime.Now.ToString(CultureInfo.InvariantCulture)))
+                {
+                    content.Add(fileName, "fileName");
+                    content.Add(fileRecord, "fileRecord");
+                    content.Add(file, "file", "dummyfileName.jpg");
+
+                    using (var message = await client.PostAsync(url, content))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+
+                        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+                    }
+                }
+            }
+
+        }
+    }
+}
